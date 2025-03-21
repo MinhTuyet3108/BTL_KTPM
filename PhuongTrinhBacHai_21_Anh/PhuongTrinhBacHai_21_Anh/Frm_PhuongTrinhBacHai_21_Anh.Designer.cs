ee82dc1d21e67742e03e2745aeb3ca9d6eeefb20
Theanh130124 <theanhtran13012004@gmail.com>
2025-03-21 10:40:23 +0700

    Update Forms and GiaiPhuongTrinhBacHai.cs

---
+++ PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHai_21_Anh/Frm_PhuongTrinhBacHai_21_Anh.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PhuongTrinhBacHai_21_Anh
+{
+    partial class Frm_PhuongTrinhBacHai_21_Anh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_PhuongTrinhBacHai_21_Anh));
+            this.lbl_name_21_Anh = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHeSoA_21_Anh = new System.Windows.Forms.TextBox();
+            this.txtHeSoB_21_Anh = new System.Windows.Forms.TextBox();
+            this.txtHeSoC_21_Anh = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnXemKQ_21_Anh = new System.Windows.Forms.Button();
+            this.lblKetQua_21_Anh = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_name_21_Anh
+            // 
+            this.lbl_name_21_Anh.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_name_21_Anh.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_name_21_Anh.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name_21_Anh.ForeColor = System.Drawing.Color.Red;
+            this.lbl_name_21_Anh.Location = new System.Drawing.Point(0, 0);
+            this.lbl_name_21_Anh.Name = "lbl_name_21_Anh";
+            this.lbl_name_21_Anh.Size = new System.Drawing.Size(1258, 101);
+            this.lbl_name_21_Anh.TabIndex = 3;
+            this.lbl_name_21_Anh.Text = "ỨNG DỤNG GIẢI PHƯƠNG TRÌNH BẬC HAI";
+            this.lbl_name_21_Anh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(595, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "* Phương trình bậc 2 có dạng ax² + bx + c = 0 (a khác 0)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "* Nhập hệ số a, b, c để tiến hành giải phương trình : ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtHeSoC_21_Anh);
+            this.groupBox1.Controls.Add(this.txtHeSoB_21_Anh);
+            this.groupBox1.Controls.Add(this.txtHeSoA_21_Anh);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(225, 314);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(811, 211);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Hệ số a :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Hệ số b :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 29);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Hệ số c :";
+            // 
+            // txtHeSoA_21_Anh
+            // 
+            this.txtHeSoA_21_Anh.Location = new System.Drawing.Point(201, 38);
+            this.txtHeSoA_21_Anh.Name = "txtHeSoA_21_Anh";
+            this.txtHeSoA_21_Anh.Size = new System.Drawing.Size(408, 22);
+            this.txtHeSoA_21_Anh.TabIndex = 3;
+            // 
+            // txtHeSoB_21_Anh
+            // 
+            this.txtHeSoB_21_Anh.Location = new System.Drawing.Point(201, 96);
+            this.txtHeSoB_21_Anh.Name = "txtHeSoB_21_Anh";
+            this.txtHeSoB_21_Anh.Size = new System.Drawing.Size(408, 22);
+            this.txtHeSoB_21_Anh.TabIndex = 4;
+            // 
+            // txtHeSoC_21_Anh
+            // 
+            this.txtHeSoC_21_Anh.Location = new System.Drawing.Point(201, 158);
+            this.txtHeSoC_21_Anh.Name = "txtHeSoC_21_Anh";
+            this.txtHeSoC_21_Anh.Size = new System.Drawing.Size(408, 22);
+            this.txtHeSoC_21_Anh.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(88, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(724, 160);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LƯU Ý";
+            // 
+            // btnXemKQ_21_Anh
+            // 
+            this.btnXemKQ_21_Anh.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnXemKQ_21_Anh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXemKQ_21_Anh.ForeColor = System.Drawing.Color.Red;
+            this.btnXemKQ_21_Anh.Location = new System.Drawing.Point(874, 551);
+            this.btnXemKQ_21_Anh.Name = "btnXemKQ_21_Anh";
+            this.btnXemKQ_21_Anh.Size = new System.Drawing.Size(162, 39);
+            this.btnXemKQ_21_Anh.TabIndex = 8;
+            this.btnXemKQ_21_Anh.Text = "Xem kết quả";
+            this.btnXemKQ_21_Anh.UseVisualStyleBackColor = false;
+            this.btnXemKQ_21_Anh.Click += new System.EventHandler(this.btnXemKQ_21_Anh_Click);
+            // 
+            // lblKetQua_21_Anh
+            // 
+            this.lblKetQua_21_Anh.AutoSize = true;
+            this.lblKetQua_21_Anh.BackColor = System.Drawing.Color.Transparent;
+            this.lblKetQua_21_Anh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua_21_Anh.ForeColor = System.Drawing.Color.Red;
+            this.lblKetQua_21_Anh.Location = new System.Drawing.Point(222, 660);
+            this.lblKetQua_21_Anh.Name = "lblKetQua_21_Anh";
+            this.lblKetQua_21_Anh.Size = new System.Drawing.Size(107, 29);
+            this.lblKetQua_21_Anh.TabIndex = 9;
+            this.lblKetQua_21_Anh.Text = "Kết quả :";
+            // 
+            // Frm_PhuongTrinhBacHai_21_Anh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1258, 778);
+            this.Controls.Add(this.lblKetQua_21_Anh);
+            this.Controls.Add(this.btnXemKQ_21_Anh);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_name_21_Anh);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Frm_PhuongTrinhBacHai_21_Anh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_PhuongTrinhBacHai_21_Anh";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_name_21_Anh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtHeSoA_21_Anh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeSoC_21_Anh;
+        private System.Windows.Forms.TextBox txtHeSoB_21_Anh;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnXemKQ_21_Anh;
+        private System.Windows.Forms.Label lblKetQua_21_Anh;
+    }
+}
+
---
+++ PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHai_21_Anh/PhepTinhPhuongTrinhBacHai_21_Anh.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuongTrinhBacHai_21_Anh
+{
+    public class PhepTinhPhuongTrinhBacHai_21_Anh // sửa internal thành public để có thể gọi class này ở project unit test (project khác)
+    {
+        public static string GiaiPhuongTrinhBacHai_21_Anh(double a_21_Anh, double b_21_Anh, double c_21_Anh) // dùng static để không cần tạo mới đối tượng ( new GiaiPhuongTrinhBacHai_21_Anh(a,b,c))
+        {
+            if (a_21_Anh == 0)
+            {
+                if ( b_21_Anh== 0)
+                {
+                    return (c_21_Anh == 0) ? "Phương trình vô số nghiệm" : "Phương trình vô nghiệm"; 
+                }
+                else
+                {
+                    double x_21_Anh = -c_21_Anh / b_21_Anh;
+                    return $"Phương trình có một nghiệm x = {x_21_Anh:F1}";
+                    //Hoặc dùng String.Format("Phương trình có một nghiệm x = {0}", x_21_Anh);
+                }
+
+            }
+            else
+            {
+                double delta_21_Anh = b_21_Anh *b_21_Anh -4 *a_21_Anh * c_21_Anh;
+                if (delta_21_Anh > 0)
+                {
+                    double x1_21_Anh = (-b_21_Anh - Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
+                    double x2_21_Anh = (-b_21_Anh + Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
+                    return $"Phương trình có hai nghiệm phân biệt x1= {x1_21_Anh:F1} , x2= {x2_21_Anh:F1}";
+                }
+                else if (delta_21_Anh == 0)
+                {
+                    double x_21_Anh = -b_21_Anh/(2 * a_21_Anh);
+                    return $"Phương trình có nghiệm kép : x = {x_21_Anh:F1}";
+                }
+                else // delta_21_Anh < 0 
+                {
+                    return "Phương trình vô nghiệm";
+                }
+            }
+        }
+    }
+}
