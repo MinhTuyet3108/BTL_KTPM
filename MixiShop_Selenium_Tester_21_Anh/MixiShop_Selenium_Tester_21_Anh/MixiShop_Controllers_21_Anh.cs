68306e6f47dad8ea1e653629711d6ebc9ad67e3d
Theanh130124 <theanhtran13012004@gmail.com>
2025-03-28 11:18:33 +0700

    Update MixiShop

---
+++ MixiShop_Selenium_Tester_21_Anh/MixiShop_Selenium_Tester_21_Anh/MixiShop_Controllers_21_Anh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Thêm dòng này để làm việc với Selenium
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MixiShop_Selenium_Tester_21_Anh
+{
+    class MixiShop_Controllers_21_Anh // không sửa public cũng được 
+    {
+        public void TruyCapTrangChu_21_Anh () { 
+            IWebDriver driver_21_Anh = new ChromeDriver ();
+            driver_21_Anh.Navigate().GoToUrl("https://shop.mixigaming.com/");
+        }
+        public void DangNhapMixiShop_21_Anh(string email_21_Anh , string matkhau_21_Anh)
+        {
+
+        }
+    }
+}
