df0aa0e3a0ba65d43cf2f9ff55e7fd21743b7f3e
Theanh130124 <theanhtran13012004@gmail.com>
2025-03-30 17:05:28 +0700

    Update Task1_Login

--- CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_Controllers_21_Anh.cs
+++ CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_Controllers_21_Anh.cs
@@ -19,30 +19,34 @@ namespace CellPhoneS_SeleniumTester_21_Anh
             driver_21_Anh.Navigate().GoToUrl("https://cellphones.com.vn/");
         }
 
-        public string DangNhapCellPhoneS_21_Anh(string email_21_Anh, string matkhau_21_Anh)
+        public string DangNhapCellPhoneS_21_Anh(string sđt_21_Anh, string matkhau_21_Anh)
         {
             TruyCapTrangChu_21_Anh();
-            // cho web dừng 1s để load trang chủ
+            // cho web dừng 2s để load trang chủ
             Thread.Sleep(2000);
-            //Click vào để hiện dropdown đăng nhập
-            driver_21_Anh.FindElement(By.Id("icon-account")).Click();
+            //Click vào để vào đăng nhập
+            driver_21_Anh.FindElement(By.ClassName("login-btn")).Click();
+            // cho web dừng 2s để load nút đăng nhập
+            Thread.Sleep(2000);
+            //Click chuyển tới trang đăng nhập
+            driver_21_Anh.FindElement(By.CssSelector(".modal-login .login-btn")).Click();
 
-            //Lấy element nút đăng nhập 
-            IWebElement element_21_Anh = driver_21_Anh.FindElement(By.LinkText("Đăng nhập"));
-            //Nhấn vào để kiểm tra thông tin đăng nhập
-            element_21_Anh.Click();
 
-            //Lấy element nhập email
-            IWebElement element_email_21_Anh = driver_21_Anh.FindElement(By.Name("email"));
+            //Lấy element nhập số điện thoại
+            IWebElement element_sđt_21_Anh = driver_21_Anh.FindElement(By.CssSelector("input[type='tel']"));
             //Gửi mật khẩu được truyền bên project test
-            element_email_21_Anh.SendKeys(email_21_Anh);
+            element_sđt_21_Anh.SendKeys(sđt_21_Anh);
             //Lấy element nhập mật khẩu
-            IWebElement element_mk_21_Anh = driver_21_Anh.FindElement(By.Name("password"));
+            IWebElement element_mk_21_Anh = driver_21_Anh.FindElement(By.CssSelector("input[type='password']"));
             element_mk_21_Anh.SendKeys(matkhau_21_Anh);
             //Lấy element button đăng nhập
-            IWebElement element_submit_21_Anh = driver_21_Anh.FindElement(By.ClassName("btn-login"));
+            IWebElement element_submit_21_Anh = driver_21_Anh.FindElement(By.ClassName("button__login"));
             element_submit_21_Anh.Click();
 
+            //Nếu đã đăng nhập ddc có thể chuyển tới trang này 
+            Thread.Sleep(2000);
+
+            driver_21_Anh.Navigate().GoToUrl("https://cellphones.com.vn/smember/");
 
             string url_hientai_21_Anh = driver_21_Anh.Url;
 
