690aeb30bd3a75c0ee29a014e5c4265404f23893
Theanh130124 <theanhtran13012004@gmail.com>
2025-03-31 16:29:32 +0700

    Update Selenium SearchProduct

--- CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_SeleniumTester_21_Anh/Task1_UnitTest_Login_21_Anh.cs
+++ CellPhoneS_SeleniumTester_21_Anh/CellPhoneS_SeleniumTester_21_Anh/Task1_UnitTest_Login_21_Anh.cs
@@ -11,8 +11,6 @@ namespace CellPhoneS_SeleniumTester_21_Anh
 
         [TestMethod]
         //TC1 sđt="0933033801"  matkhau =  "tta1301"  -> kết quả : Pass
-
-
         public void TC1_DangNhap_sđtDung_matkhauDung_Pass_21_Anh()
         {
             string sđt_21_Anh = "0933033801";
@@ -38,27 +36,27 @@ namespace CellPhoneS_SeleniumTester_21_Anh
             
             Assert.AreEqual(expected_url_21_Anh, actual_url_21_Anh);
         }
-        //TC3 " -> ket qua : Pass
-        public void TC3_DangNhap_sđtSai_matkhauDung_Pass_21_Anh()
-        {
-            string sđt_21_Anh = "abc123@gmail.com";
-            string matkhau_21_Anh = "tta130124";
-            //Endpoint mà kết quả thực sự trả về
-            string actual_url_21_Anh = c_21_Anh.DangNhapCellPhoneS_21_Anh(sđt_21_Anh, matkhau_21_Anh);
-            //Endpoint mà kết quả mong đợi
-            string expected_url_21_Anh = "https://shop.mixigaming.com/account/login";
-            Assert.AreEqual(expected_url_21_Anh, actual_url_21_Anh);
-        }
-        //TC4  -> ket qua : Pass
-        public void TC4_DangNhap_sđtSai_matkhauSai_Pass_21_Anh()
-        {
-            string sđt_21_Anh = "abc123@gmail.com";
-            string matkhau_21_Anh = "tta130124";
-            //Endpoint mà kết quả thực sự trả về
-            string actual_url_21_Anh = c_21_Anh.DangNhapCellPhoneS_21_Anh(sđt_21_Anh, matkhau_21_Anh);
-            //Endpoint mà kết quả mong đợi
-            string expected_url_21_Anh = "https://shop.mixigaming.com/account/login";
-            Assert.AreEqual(expected_url_21_Anh, actual_url_21_Anh);
-        }
+        ////TC3 " -> ket qua : Pass
+        //public void TC3_DangNhap_sđtSai_matkhauDung_Pass_21_Anh()
+        //{
+        //    string sđt_21_Anh = "abc123@gmail.com";
+        //    string matkhau_21_Anh = "tta130124";
+        //    //Endpoint mà kết quả thực sự trả về
+        //    string actual_url_21_Anh = c_21_Anh.DangNhapCellPhoneS_21_Anh(sđt_21_Anh, matkhau_21_Anh);
+        //    //Endpoint mà kết quả mong đợi
+        //    string expected_url_21_Anh = "https://shop.mixigaming.com/account/login";
+        //    Assert.AreEqual(expected_url_21_Anh, actual_url_21_Anh);
+        //}
+        ////TC4  -> ket qua : Pass
+        //public void TC4_DangNhap_sđtSai_matkhauSai_Pass_21_Anh()
+        //{
+        //    string sđt_21_Anh = "abc123@gmail.com";
+        //    string matkhau_21_Anh = "tta130124";
+        //    //Endpoint mà kết quả thực sự trả về
+        //    string actual_url_21_Anh = c_21_Anh.DangNhapCellPhoneS_21_Anh(sđt_21_Anh, matkhau_21_Anh);
+        //    //Endpoint mà kết quả mong đợi
+        //    string expected_url_21_Anh = "https://shop.mixigaming.com/account/login";
+        //    Assert.AreEqual(expected_url_21_Anh, actual_url_21_Anh);
+        //}
     }
 }
