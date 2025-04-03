9cc625a90bdb6ccb07bac59f9a09062904c0a2ba
MinhTuyet3108 <ntmtuyet04vn@gmail.com>
2025-04-03 16:52:27 +0700

    update invalid

--- PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHaiTester_82_Tuyet/UnitTest_PTBacHai_82_Tuyet.cs
+++ PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHaiTester_82_Tuyet/UnitTest_PTBacHai_82_Tuyet.cs
@@ -16,12 +16,9 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
         string Ketqua_82_Tuyet;
 
 
-        //TC1 : a=0.0, b=2.0, c=6.0, ket qua mong doi = {-3.0} -> Pass do kết quả thật sự cũng là 
-
         public TestContext TestContext { get; set; }
 
-
-
+        //TC1 : a=0.0, b=2.0, c=6.0, ket qua mong doi = {-3.0} -> Pass do kết quả thật sự cũng là {-3.0}
         [TestMethod]
         public void TC1_a_bangkhong_b_c_khackhong_Pass_82_Tuyet()
         {
@@ -35,7 +32,7 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
 
         [TestMethod]
 
-        //TC2 : a=0.0, b=0.0, c=4.0, ket qua mong doi = { } (vô nghiệm) -> Pass do kết quả thật sự cũng là
+        //TC2 : a=0.0, b=0.0, c=4.0, ket qua mong doi = { } (vô nghiệm) -> Pass do kết quả thật sự cũng là { }
 
         public void TC2_a_b_bangkhong_c_khackhong_Pass_82_Tuyet()
         {
@@ -49,7 +46,7 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
 
         [TestMethod]
 
-        //TC3 : a=0.0, b=0.0, c=0.0, ket qua mong doi = { } (vô số nghiệm) -> Pass do kết quả thật sự cũng là
+        //TC3 : a=0.0, b=0.0, c=0.0, ket qua mong doi = { } (vô số nghiệm) -> Pass do kết quả thật sự cũng là { }
 
         public void TC3_a_b_c_bangkhong_Pass_82_Tuyet()
         {
@@ -62,7 +59,7 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
         }
         [TestMethod]
 
-        //TC4 : a=0.0, b=5.0, c=15.0, ket qua mong doi ={ 4.0} -> Fail do kết quả thật sự là
+        //TC4 : a=0.0, b=5.0, c=15.0, ket qua mong doi ={ 4.0} -> Fail do kết quả thật sự là {-3.0}
 
         public void TC4_a_bangkhong_b_c_khackhong_Fail_82_Tuyet()
         {
@@ -76,7 +73,7 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
 
         [TestMethod]
 
-        //TC5 : a=0.0, b=0.0, c=4.0, ket qua mong doi = { 5.0} -> Fail do kết quả thật sự là
+        //TC5 : a=0.0, b=0.0, c=4.0, ket qua mong doi = { 5.0} -> Fail do kết quả thật sự là { }
 
         public void TC5_a_b_bangkhong_c_khackhong_Fail_82_Tuyet()
         {
@@ -92,7 +89,7 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
 
         [TestMethod]
 
-        //TC6 : a=0.0, b=0.0, c=0.0, ket qua mong doi = {3.0 }  -> Fail do kết quả thật sự là
+        //TC6 : a=0.0, b=0.0, c=0.0, ket qua mong doi = {3.0 }  -> Fail do kết quả thật sự là { }
 
         public void TC6_a_b_c_bangkhong_Fail_82_Tuyet()
         {
@@ -178,56 +175,76 @@ namespace PhuongTrinhBacHaiTester_82_Tuyet
 
         }
 
-
-
-
-        //TC13 -> DataSource -> 12 test case -> 6 pass - 6 fail 
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
         [TestMethod]
 
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestPower.csv", "TestPower#csv", DataAccessMethod.Sequential)]
+        //TC13 test với dữ liệu csv -> DataSource -> 12 test case -> 6 pass - 6 fail 
+        //a,b,c,expected_x1,expected_x2
+        //1,-2,1,1,1        -> Pass - Nghiệm kép tại x = 1
+        //2,-4,2,1,1        -> Pass - Nghiệm kép tại x = 1
+        //1,-3,2,2,1        -> Pass - Hai nghiệm phân biệt tại x1 = 1, x2 = 2
+        //2,-8,6,3,1        -> Pass - Hai nghiệm phân biệt tại x1 = 1, x2 = 3
+        //1,4,4,-2,-2       -> Pass - Nghiệm kép tại x = -2
+        //1,2,5,NULL,NULL   -> Pass - Phương trình vô nghiệm
+
+        //1,2,5,5,5         -> Fail - Vô nghiệm nhưng lại có nghiệm thực không hợp lệ
+        //1,-4,4,3,2        -> Fail - Nghiệm phân biệt không khớp, phải là x1 = 2, x2 = 2
+        //2,-6,5,5,7        -> Fail - Nghiệm kép nhưng lại trả về x2=7
+        //4,-8,4,2,1        -> Fail - Nghiệm kép tại x = 2 nhưng lại có x2 khác 2
+        //1,2,3,1,1         -> Fail - Nghiệm phân biệt nhưng lại trả về nghiệm kép tại x = 1
+        //1,-3,2,3,1        -> Fail - Hai nghiệm phân biệt nhưng x1 và x2 không đúng
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data_82_Tuyet\TestDataPTBH_82_Tuyet.csv", "TestDataPTBH_82_Tuyet#csv", DataAccessMethod.Sequential)]
         public void TC13_12Testcase_6pass_6fail_82_Tuyet()
         {
 
+            double a_82_Tuyet, b_82_Tuyet, c_82_Tuyet;
+            double[] expected_82_Tuyet;
+
+
+            double.TryParse(TestContext.DataRow[0].ToString(), out a_82_Tuyet);
+            double.TryParse(TestContext.DataRow[1].ToString(), out b_82_Tuyet);
+            double.TryParse(TestContext.DataRow[2].ToString(), out c_82_Tuyet);
+
+            string x1_str_82_Tuyet = TestContext.DataRow[3].ToString();
+            string x2_str_82_Tuyet = TestContext.DataRow[4].ToString();
+
+            if (x1_str_82_Tuyet == "NULL" && x2_str_82_Tuyet == "NULL")
+            {
+                expected_82_Tuyet = new double[] { };
+
+            }
+            else if (x1_str_82_Tuyet == x2_str_82_Tuyet && double.TryParse(x1_str_82_Tuyet, out double x1_82_Tuyet))
+            {
+                //double.TryParse(x1_str_82_Tuyet, out double x1_82_Tuyet);
+                expected_82_Tuyet = new double[] { x1_82_Tuyet };
+            }
+            else if(double.TryParse(x1_str_82_Tuyet, out x1_82_Tuyet)&&double.TryParse(x2_str_82_Tuyet, out double x2_82_Tuyet))
+            {
+                //double x1_82_Tuyet = double.Parse(x1_str_82_Tuyet);
+                //double x2_82_Tuyet = double.Parse(x2_str_82_Tuyet);
+                expected_82_Tuyet = new double[] { x1_82_Tuyet, x2_82_Tuyet };
+            }
+            else
+                expected_82_Tuyet = new double[] {};
+            double[] Nghiem_82_Tuyet;
+            string Ketqua_82_Tuyet;
+
+            PhepTinhPhuongTrinhBacHai_21_Anh.GiaiPhuongTrinhBacHai_21_Anh(out Nghiem_82_Tuyet, out Ketqua_82_Tuyet, a_82_Tuyet, b_82_Tuyet, c_82_Tuyet);
+            double[] actual_82_Tuyet = Nghiem_82_Tuyet;
+            CollectionAssert.AreEqual(expected_82_Tuyet, actual_82_Tuyet, $"Expected: [{string.Join(", ", expected_82_Tuyet)}], Actual: [{string.Join(", ", actual_82_Tuyet)}]");
         }
 
-
-    //TC14 ở đây
-
-
-
-
-        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data_82_Tuyet\TestDataPTBH_82_Tuyet.csv", "TestDataPTBH_82_Tuyet#csv", DataAccessMethod.Sequential)]
-        //public void TC13_12Testcase_6pass_6fail_82_Tuyet()
-        //{
-
-        //    double a_82_Tuyet, b_82_Tuyet, c_82_Tuyet;
-        //    double x1_82_Tuyet, x2_82_Tuyet;
-        //    a_82_Tuyet = double.Parse(TestContext.DataRow[0].ToString());
-        //    b_82_Tuyet = double.Parse(TestContext.DataRow[1].ToString());
-        //    c_82_Tuyet = double.Parse(TestContext.DataRow[2].ToString());
-        //    x1_82_Tuyet = double.Parse(TestContext.DataRow[3].ToString());
-        //    x2_82_Tuyet = double.Parse(TestContext.DataRow[4].ToString());
-        //    double[] expected_82_Tuyet = {x1_82_Tuyet, x2_82_Tuyet};
-        //    PhepTinhPhuongTrinhBacHai_21_Anh.GiaiPhuongTrinhBacHai_21_Anh(out Nghiem_82_Tuyet, out Ketqua_82_Tuyet, a_82_Tuyet, b_82_Tuyet, c_82_Tuyet);
-        //    double[] actual_82_Tuyet = Nghiem_82_Tuyet;
-        //    CollectionAssert.AreEqual(expected_82_Tuyet, actual_82_Tuyet, $"Expected: [{string.Join(", ", expected_82_Tuyet)}], Actual: [{string.Join(", ", actual_82_Tuyet)}]");
-        //}
-
+        //TC14 nhận biết lỗi khi người dùng nhập sai định dạng, a="abc", b="xyz", c="!@#"
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TC14_loiDinhDang_82_Tuyet_Pass()
+        {
+            // giả lập dữ liệu sai kiểu
+            double a_82_Tuyet = double.Parse("abc");
+            double b_82_Tuyet = double.Parse("xyz"); 
+            double c_82_Tuyet = double.Parse("!@#");
+            PhepTinhPhuongTrinhBacHai_21_Anh.GiaiPhuongTrinhBacHai_21_Anh(out Nghiem_82_Tuyet, out Ketqua_82_Tuyet, a_82_Tuyet, b_82_Tuyet, c_82_Tuyet);
+        }
 
     }
 }
--- PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHai_21_Anh/PhepTinhPhuongTrinhBacHai_21_Anh.cs
+++ PhuongTrinhBacHai_21_Anh/PhuongTrinhBacHai_21_Anh/PhepTinhPhuongTrinhBacHai_21_Anh.cs
@@ -7,65 +7,68 @@ using System.Threading.Tasks;
 namespace PhuongTrinhBacHai_21_Anh
 {
     // sửa internal thành public để có thể gọi class này ở project unit test (project khác)
-    public class PhepTinhPhuongTrinhBacHai_21_Anh 
+    public class PhepTinhPhuongTrinhBacHai_21_Anh
     {
         //Dùng out parameter để xử lý riêng bên form và bên tester (bên form muốn hiện kết quả có cả chuỗi , nhưng tester lại chỉ muốn test kết quả là mảng double nghiệm)
         // dùng static để không cần tạo mới đối tượng (không cần gọi GiaiPhuongTrinhBacHai_21_Anh c_21_Anh =  new GiaiPhuongTrinhBacHai_21_Anh(a,b,c))
-        public static void GiaiPhuongTrinhBacHai_21_Anh(out double[] Nghiem_21_Anh , out string KetQua_21_Anh ,  double a_21_Anh, double b_21_Anh, double c_21_Anh) 
+        public static void GiaiPhuongTrinhBacHai_21_Anh(out double[] Nghiem_21_Anh, out string KetQua_21_Anh, double a_21_Anh, double b_21_Anh, double c_21_Anh)
 
         {
-            try
-            {
-                if (a_21_Anh == 0)
-                {
-                    if (b_21_Anh == 0)
-                    {
-                        KetQua_21_Anh = (c_21_Anh == 0) ? "Phương trình vô số nghiệm" : "Phương trình vô nghiệm";
-                        Nghiem_21_Anh = new double[] { };
-                    }
-                    else
-                    {
-
-                        double x_21_Anh = -c_21_Anh / b_21_Anh;
-                        KetQua_21_Anh = String.Format("Phương trình có một nghiệm x = {0:F2}", x_21_Anh);
-                        Nghiem_21_Anh = new double[] { x_21_Anh };
-
-                    }
-
-                }
-                else
-                {
-                    double delta_21_Anh = b_21_Anh * b_21_Anh - 4 * a_21_Anh * c_21_Anh;
-                    if (delta_21_Anh > 0)
-                    {
-                        double x1_21_Anh = (-b_21_Anh - Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
-                        double x2_21_Anh = (-b_21_Anh + Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
-                        KetQua_21_Anh = String.Format("Phương trình có hai nghiệm phân biệt x1 = {0:F2} , x2 = {1:F2}", x1_21_Anh, x2_21_Anh);
-                        Nghiem_21_Anh = new double[] { x1_21_Anh, x2_21_Anh };
-
-                    }
-                    else if (delta_21_Anh == 0)
-                    {
-                        double x_21_Anh = -b_21_Anh / (2 * a_21_Anh);
-                        KetQua_21_Anh = String.Format("Phương trình có nghiệm kép : x = {0:F2}", x_21_Anh);
-                        Nghiem_21_Anh = new double[] { x_21_Anh };
-
-
-                    }
-                    // delta_21_Anh < 0 
-                    else
-                    {
-                        KetQua_21_Anh = "Phương trình vô nghiệm";
-                        Nghiem_21_Anh = new double[] { };
-                    }
-                }
-            }
-            //Khi không nhập số sẽ quăng ra ngoại lệ này
-            catch (FormatException)
+            // Nếu a b c không phải là số hợp lệ -> có thể là chử hoặc ký tự đặc biệt thì sẽ chạy điều kiện này
+            if (double.IsNaN(a_21_Anh) || double.IsNaN(b_21_Anh) || double.IsNaN(c_21_Anh))
             {
                 throw new FormatException("Dữ liệu không hợp lệ. Vui lòng nhập số.");
             }
 
+            if (a_21_Anh == 0)
+            {
+                if (b_21_Anh == 0)
+                {
+                    KetQua_21_Anh = (c_21_Anh == 0) ? "Phương trình vô số nghiệm" : "Phương trình vô nghiệm";
+                    Nghiem_21_Anh = new double[] { };
+                }
+                else
+                {
+
+                    double x_21_Anh = -c_21_Anh / b_21_Anh;
+                    KetQua_21_Anh = String.Format("Phương trình có một nghiệm x = {0:F2}", x_21_Anh);
+                    Nghiem_21_Anh = new double[] { x_21_Anh };
+
+                }
+
+            }
+            else
+            {
+                double delta_21_Anh = b_21_Anh * b_21_Anh - 4 * a_21_Anh * c_21_Anh;
+                if (delta_21_Anh > 0)
+                {
+                    double x1_21_Anh = (-b_21_Anh - Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
+                    double x2_21_Anh = (-b_21_Anh + Math.Sqrt(delta_21_Anh)) / (2 * a_21_Anh);
+                    KetQua_21_Anh = String.Format("Phương trình có hai nghiệm phân biệt x1 = {0:F2} , x2 = {1:F2}", x1_21_Anh, x2_21_Anh);
+                    Nghiem_21_Anh = new double[] { x1_21_Anh, x2_21_Anh };
+
+                }
+                else if (delta_21_Anh == 0)
+                {
+                    double x_21_Anh = -b_21_Anh / (2 * a_21_Anh);
+                    KetQua_21_Anh = String.Format("Phương trình có nghiệm kép : x = {0:F2}", x_21_Anh);
+                    Nghiem_21_Anh = new double[] { x_21_Anh };
+
+
+                }
+                // delta_21_Anh < 0 
+                else
+                {
+                    KetQua_21_Anh = "Phương trình vô nghiệm";
+                    Nghiem_21_Anh = new double[] { };
+                }
+            }
         }
+
+        //Khi không nhập số sẽ quăng ra ngoại lệ này
+
+
     }
 }
+
+//}
